4a82b8fb9c81a331021afa242c90d377a9bc3f56
farinman <mfarinaboullosa@gmail.com>
2019-06-03 21:45:13 +0200

    changed variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD-Cwx7-Lc1F0kglauHYnUeYMGB-Mm8c4Q',
+    authDomain: 'redcross-ama.firebaseapp.com',
+    databaseURL: 'https://redcross-ama.firebaseio.com',
+    projectId: 'redcross-ama',
+    storageBucket: 'redcross-ama.appspot.com',
+    messagingSenderId: '71549541082'
+  }
 };
