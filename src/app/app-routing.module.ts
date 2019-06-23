dc91c8474f1448807c5496d454487ba3cb8a2eff
farinman <mfarinaboullosa@gmail.com>
2019-06-23 18:18:36 +0200

    deleted redundant code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegistrationComponent } from './auth/registration/registration.component';
 
-
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent},
